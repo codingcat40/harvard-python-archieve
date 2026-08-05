9117050804cafd22f91731f76f664ee46c40c029
Naman Matoliya <naman.matoliya50962@gmail.com>
2026-08-05 16:53:13 +0530

    global variables

---
+++ final_section_python/houses.py
@@ -0,0 +1,19 @@
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+    
+]
+
+houses = set()
+
+for student in students:
+        houses.add(student["house"])
+
+for house in sorted(houses):
+    print(house)
+    
+if "Slytherin" in houses:
+     print(True)
